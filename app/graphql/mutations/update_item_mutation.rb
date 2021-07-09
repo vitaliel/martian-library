38373a4039358d2906290d7f336180696b54ad01
Vitalie Lazu <vitalie.lazu@gmail.com>
2021-07-09 15:24:33 +0300

    DRY item attributes

--- app/graphql/mutations/update_item_mutation.rb
+++ app/graphql/mutations/update_item_mutation.rb
@@ -1,19 +1,17 @@
 module Mutations
   class UpdateItemMutation < BaseMutation
     argument :id, ID, required: true
-    argument :title, String, required: true
-    argument :description, String, required: false
-    argument :image_url, String, required: false
+    argument :attributes, Types::ItemAttributes, required: true
 
     field :item, Types::ItemType, null: true
     field :errors, Types::ValidationErrorsType, null: true
 
-    def resolve(id:, title:, description: nil, image_url: nil)
+    def resolve(id:, attributes:)
       check_authentication!
 
       item = Item.find(id)
 
-      if item.update(title: title, description: description, image_url: image_url)
+      if item.update(attributes.to_h)
         { item: item }
       else
         { errors: item.errors }

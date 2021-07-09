927b40028605abd896c9fc05bb07b96d5a139599
Vitalie Lazu <vitalie.lazu@gmail.com>
2021-07-09 11:37:01 +0300

    part2: add/update item

--- app/javascript/components/Library/index.js
+++ app/javascript/components/Library/index.js
@@ -1,31 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import { Query } from "react-apollo";
-import gql from 'graphql-tag';
 
-const LibraryQuery = gql`
-{
-  items {
-    id
-    title
-    user {
-      email
-    }
-  }
-}
-`;
+import { LibraryQuery } from './operations.graphql';
+import UpdateItemForm from "../UpdateItemForm";
 
-export default () => (
-  <Query query={LibraryQuery}>
-    {({data, loading}) => (
-      <div>
-        {loading
-          ? 'loading...'
-          : data.items.map(({title, id, user}) => (
-            <div key={id}>
-              <b>{title}</b> {user ? `added by ${user.email}` : null}
-            </div>
-          ))}
-      </div>
-    )}
-  </Query>
-);
+import cs from './styles';
+
+export default () => {
+  const [item, setItem] = useState(null);
+
+  return (
+    <Query query={LibraryQuery}>
+      {({ data, loading }) => (
+        <div className={cs.library}>
+          {loading || !data.items
+            ? 'loading...'
+            : data.items.map(({ title, id, user, imageUrl, description }) => (
+              <button
+                key={id}
+                className={cs.plate}
+                onClick={() => setItem({ title, id, imageUrl, description })}
+              >
+                <div className={cs.title}>{title}</div>
+                <div>{description}</div>
+                {imageUrl && <img src={imageUrl} className={cs.image}/>}
+                {user ? (
+                  <div className={cs.user}>added by {user.email}</div>
+                ) : null}
+              </button>
+            ))}
+          {item != null && (
+            <UpdateItemForm
+              id={item.id}
+              initialTitle={item.title}
+              initialDescription={item.description}
+              initialImageUrl={item.imageUrl}
+              onClose={() => setItem(null)}
+            />
+          )}
+        </div>
+      )}
+    </Query>
+  );
+};
